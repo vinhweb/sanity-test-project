ac2cd9c2bf4e658395d4f26b115e71ce601f8ae1
vinhweb <vjhnguyen@gmail.com>
2024-10-21 10:31:39 +0700

    structure customization

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {structure} from './structure'
+import {defaultDocumentNode} from './structure/defaultDocumentNode'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,10 @@ export default defineConfig({
   projectId: 'fcrz7blc',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({
+    structure,
+    defaultDocumentNode
+  }), visionTool()],
 
   schema: {
     types: schemaTypes,
